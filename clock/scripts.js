e9227d3ec6f085b55f63320baea0e7d7310bd8b5
Michael Harry <michael@compilefailed.com>
2025-02-01 21:34:47 -0600

    Add mechanism for inputting test times via hash

--- clock/scripts.js
+++ clock/scripts.js
@@ -1,8 +1,14 @@
 (function(){
+    var overrideTime = null;
+
+    if (window.location.hash.startsWith("#time")){
+        overrideTime = new Date(decodeURIComponent(window.location.hash.split('=')[1]));
+    }
+
     const formatLeadingZero = (number) => String(number).padStart(2, '0');
     const localTimeZoneAbbreviation = Intl.DateTimeFormat('en-US', { timeZoneName: 'short' }).formatToParts(new Date()).find(part => part.type == 'timeZoneName').value;
     const renderTime = () => {
-        let time = new Date();
+        let time = overrideTime ?? new Date();
         let anteMeridian = (time.getHours / 12) > 1;
         document.querySelector('#local-clock-display').innerHTML = `${formatLeadingZero(time.getMonth() + 1)}-${formatLeadingZero(time.getDate())}-${formatLeadingZero(time.getFullYear())} ${formatLeadingZero(time.getHours() % 12)}:${formatLeadingZero(time.getMinutes())}:${formatLeadingZero(time.getSeconds())} ${anteMeridian ? "AM" : "PM"} ${localTimeZoneAbbreviation}`;
         document.querySelector('#utc-clock-display').innerHTML = `${formatLeadingZero(time.getUTCMonth() + 1)}-${formatLeadingZero(time.getUTCDate())}-${formatLeadingZero(time.getUTCFullYear())} ${formatLeadingZero(time.getUTCHours())}:${formatLeadingZero(time.getUTCMinutes())}:${formatLeadingZero(time.getUTCSeconds())} UTC`;
