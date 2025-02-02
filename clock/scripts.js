79225fa823bc76de0b9bdaa067774fd2c6f8a7e4
Michael Harry <michael@compilefailed.com>
2025-02-01 21:46:36 -0600

    Fix issue where 12 AM or PM would display as 00

--- clock/scripts.js
+++ clock/scripts.js
@@ -7,10 +7,19 @@
 
     const formatLeadingZero = (number) => String(number).padStart(2, '0');
     const localTimeZoneAbbreviation = Intl.DateTimeFormat('en-US', { timeZoneName: 'short' }).formatToParts(new Date()).find(part => part.type == 'timeZoneName').value;
+    
+    const format12HourHour = (twentyFourHourHour) => {
+        let hour = twentyFourHourHour % 12;
+        if (hour == 0){
+            hour = 12;
+        }
+        return hour;
+    }
+    
     const renderTime = () => {
         let time = overrideTime ?? new Date();
         let anteMeridian = (time.getHours() / 12) < 1;
-        document.querySelector('#local-clock-display').innerHTML = `${formatLeadingZero(time.getMonth() + 1)}-${formatLeadingZero(time.getDate())}-${formatLeadingZero(time.getFullYear())} ${formatLeadingZero(time.getHours() % 12)}:${formatLeadingZero(time.getMinutes())}:${formatLeadingZero(time.getSeconds())} ${anteMeridian ? "AM" : "PM"} ${localTimeZoneAbbreviation}`;
+        document.querySelector('#local-clock-display').innerHTML = `${formatLeadingZero(time.getMonth() + 1)}-${formatLeadingZero(time.getDate())}-${formatLeadingZero(time.getFullYear())} ${formatLeadingZero(format12HourHour(time.getHours()))}:${formatLeadingZero(time.getMinutes())}:${formatLeadingZero(time.getSeconds())} ${anteMeridian ? "AM" : "PM"} ${localTimeZoneAbbreviation}`;
         document.querySelector('#utc-clock-display').innerHTML = `${formatLeadingZero(time.getUTCMonth() + 1)}-${formatLeadingZero(time.getUTCDate())}-${formatLeadingZero(time.getUTCFullYear())} ${formatLeadingZero(time.getUTCHours())}:${formatLeadingZero(time.getUTCMinutes())}:${formatLeadingZero(time.getUTCSeconds())} UTC`;
         document.querySelector('#unix-clock-display').innerHTML = Math.round(time.getTime() / 1000);
     }
